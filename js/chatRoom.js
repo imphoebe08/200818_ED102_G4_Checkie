9307093d856f0207ec0f2d7eebe920aa8bdc5141
Phoebe Tsai <imphoebe08@gmail.com>
2020-08-18 20:48:58 +0800

    no message

--- js/chatRoom.js
+++ js/chatRoom.js
@@ -1,4 +1,4 @@
-
+// 左側醫師區塊
 Vue.component('doctors', {
     props: ['doctor'],
     template: `
@@ -17,6 +17,38 @@ Vue.component('doctors', {
     },
 });
 
+// User對話框
+Vue.component('user-chatbox', {
+    props:['text'],
+    template: `
+    <div class="chatRoom_userConver">
+    <div class="chatRoom_conver--more"><img src="./img/icon/conver_more.svg" alt=""></div>
+    <div class="chatRoom_conver--userText">{{text}}</div>
+    <div class="chatRoom_conver--userPic"><img src="./img/chatRoom/userPic.png" alt=""></div>
+    
+    </div>`,
+    methods: {
+        
+    },
+});
+
+
+// 醫師對話窗
+
+Vue.component('doctor-chatbox', {
+    props:['text'],
+    template: `
+    <div class="chatRoom_drConver">
+    <div class="chatRoom_conver--drPic"><img src="./img/chatRoom/drPic.png" alt=""></div>
+    <div class="chatRoom_conver--drText">{{text}}</div>
+    <div class="chatRoom_conver--more"><img src="./img/icon/conver_more.svg" alt=""></div>
+    </div>`,
+    methods: {
+        
+    },
+});
+
+// 呼叫Vue
 new Vue({
     el:"#chatRoom",
 })
