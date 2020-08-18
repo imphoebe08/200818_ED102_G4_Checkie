329a8c9018d2f9a8bb0b522441f72bb0306f4770
Phoebe Tsai <imphoebe08@gmail.com>
2020-08-18 20:24:35 +0800

    no message

--- js/chatRoom.js
+++ js/chatRoom.js
@@ -1,10 +1,22 @@
+
+Vue.component('doctors', {
+    props: ['doctor'],
+    template: `
+    <div class="chatRoom_drChatbox">
+    <div class="chatRoom_light"></div>
+    <div class="chatRoom_drPic">
+        <img src="./img/chatRoom/drPic.png" alt="">
+    </div>
+    <h5>{{doctor}}</h5>
+    <div class="chatRoom_more">
+        <img src="./img/icon/more.svg" alt="">
+    </div>
+    </div>`,
+    methods: {
+        
+    },
+});
+
 new Vue({
     el:"#chatRoom",
-    data: {
-        Cinfor:{
-            img:"#",
-            name:"妊娠紋",
-            last_content:"安安",
-        }
-    },
 })
