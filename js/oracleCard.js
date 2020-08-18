3574a0ce11c904289d581cbe0cd6d16d5cfa0135
a0987123023 <a0987123023@gmail.com>
2020-08-18 16:02:03 +0800

    no message

--- js/oracleCard.js
+++ js/oracleCard.js
@@ -51,6 +51,13 @@ function doFirst(){
 window.addEventListener('load',doFirst);
 
 
-
+function getScreenshot(){
+  html2canvas($('#cutScreen'),{
+    onrendered: function(canvas){
+      // $('#cutPhoto').html("");
+      $('#cutPhoto').append(canvas);
+    }
+  });
+}
 
 
