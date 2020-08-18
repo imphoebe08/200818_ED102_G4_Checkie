ca345a89c914ef7204bbf6f2efe64969f6b5fdf0
Phoebe Tsai <imphoebe08@gmail.com>
2020-08-18 16:11:05 +0800

    acSelfCard motified

--- js/acSelf_acRelateCard.js
+++ js/acSelf_acRelateCard.js
@@ -17,14 +17,19 @@ new Vue({
             <!-- 卡片時間 -->
             <div class="acRelateCard_text">
                 <img class="acRelateCard-time_icon"src="./img/icon/clock.png" alt="">
-                <p class="acRelateCard_time">2020-08-15（六）</p>
+                <p class="acRelateCard_time">2020-08-15（六）<br> 14:00 - 16:00</p>
+                <div class="acRelateCard_icon">
                 <img class="acRelateCard-share_icon"src="./img/icon/share.png" alt="">
                 <img class="acRelateCard-bookmark_icon"src="./img/icon/bookmark.png" alt="">
+                </div>
             </div>
             
             <!-- 卡片文字 -->
             <h6 class="acRelateCard_title"><a href="./ac_content.html">信念探索團：親密關係</a></h6>
-            <p class="acRelateCard_category">#親密關係</p>
+            <div class="acRelateCard_bottomBlock">
+            <p class="acRelateCard_person"> 剩餘名額：10人<p>
+            <input id="acRelateCard_register" type="button" value="立即報名" class="acRelateCard_register">
+            </div>
         </div>
             `,
         },
