5ea2d8a173d625ee9b422fd495d1d7a89f387ef0
u10210018 <u10210018@go.utaipei.edu.tw>
2020-08-21 10:33:39 +0800

    csMain csSelf 082101

--- js/csSelf.js
+++ js/csSelf.js
@@ -1,4 +1,4 @@
-(function () {
+function drawCharts() {
     let ctx = document.getElementById("myChart").getContext("2d");
     let datas = {
         labels: ["Coding", "Swimming", "Eating", "Cycling", "Sleeping"],
@@ -30,17 +30,22 @@
         data: datas,
         options: option,
     });
-})();
+};
 
-(function () {
+drawCharts();
+
+function slideList() {
     let jsList = document.getElementsByClassName('js-list-toggle');
     let jsItem = document.getElementsByClassName('js-item-toggle');
     for (let i = 0; i < jsList.length; i++) {
         jsList[i].addEventListener('click', function () {
+            console.log('hi');
             $(jsItem[i]).slideToggle();
         });
     }
-})();
+};
+console.log('hi');
+slideList();
 
 Vue.component('cssart-layout', {
     template: `<div class="csS-art__card">
@@ -54,10 +59,8 @@ Vue.component('cssart-layout', {
                     </div>
                     <p @click="openArtPage">因感情問題來看診的年輕女性，有這四個共同特點</p>
                     <p class="small" @click="openArtPage">不管對男生、女生來說，失去身邊親愛的人絕對是很大的打擊，其實跟年紀也不一定有關係。我有一個50多歲的病人，陷入一場瘋狂的戀愛，所有的理智都沒用，憂鬱了好幾個月。</p>
-                    <div class="row justify-content-center card-tag">
-                        <a href="" class="button3-1">精神科</a>
-                        <a href="" class="button3-1">失戀</a>
-                        <a href="" class="button3-1">More..</a>
+                    <div class="row card-tag">
+                        <a href="" class="button2">More...</a>
                     </div>
                     <div class="card-share">
                         <span>Share : </span>
@@ -73,9 +76,31 @@ Vue.component('cssart-layout', {
     }
 })
 
+Vue.component("csselfact-layout", {
+    template: `
+	<div class="csSelfAct-card row">
+		<div class="card-image col-3">
+			<img src="https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/bf428880911095.5d4add485923f.jpg" alt="">
+		</div>
+		<div class="card-info col-9">
+			<h5>夏日玩水消暑--象鼻岩獨木舟</h5>
+			<span>2020/09/30</span>
+			<span>14:00~17:00</span>
+			<div class="share-buttons">
+                    <span>Share : </span>
+                    <a href="" class="small"><img src="img//icon//facebook.png" alt=""></a>
+                    <a href="" class="small"><img src="img//icon//share.png" alt=""></a>
+                    <a href="" class="small"><img src="img//icon//bookmark.png" alt=""></a>
+			</div>
+			<p>象鼻岩是位於新北瑞芳的深澳岬角，有著高約30公尺的巨大海蝕洞，因為遠看就像是個巨大的象鼻而得名，周圍有大大小小的岩石群、垂直的峭壁，其實非常適合體驗刺激的獨木舟，感受不同角度被群岩環繞的的震撼感。</p>
+		</div>
+		
+		<button class="joinBut button2">立即報名</button>
+	</div>
+`
+});
 
-
-let vm = new Vue({
+let vmcssart = new Vue({
     el: "#app",
     methods: {
         openOrderPage() {
@@ -83,3 +108,22 @@ let vm = new Vue({
         }
     }
 })
+
+
+// $(".csSelfReco-card").mouseenter(cardInfoToggle);
+
+// $(".csSelfReco-card").mouseleave(cardInfoToggle);
+
+// $(window).resize(function () {
+//     if (window.innerWidth > 800 || window.innerWidth < 576) {
+//         $(".csSelfReco-card").find(".card__info").show();
+//     } else {
+//         $(".csSelfReco-card").find(".card__info").hide();
+//     }
+// });
+
+// function cardInfoToggle(e) {
+//     if (window.innerWidth < 800 && window.innerWidth > 576) {
+//         $(this).find(".card__info").slideToggle();
+//     }
+// }
