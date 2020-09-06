5bb2c2f2e57c24f0aa5bad1ea66f407afddc3860
Phoebe Tsai <imphoebe08@gmail.com>
2020-09-06 23:16:23 +0800

    增加acMain_comment.json

--- js/acMain.js
+++ js/acMain.js
@@ -1,14 +1,16 @@
 // acMian_acCommentCard-活動好評
 Vue.component('acCommentList', {
-    props: ['doctor'],
+    props: { acComments: Array },
     template: `
-        <div class="acCommentList">
-        <div class="acCommentList_img">
-            <img src="./img/acMain/acComment.jpg" alt="">
-        </div>
-        <div class="acCommentList-text">
-            <h6>{{doctor}} ｜ 醫師</h6>
-            <p >中國家長對課外活動並不陌生，很多人都從孩子很小的時候就投入大量時間和財力，發展他們各種才藝。但是如何在大學升學背景下看待高中課外活動，值得探討。美國高中教學形式非常豐富，講課、實驗、小組項目、上台演示、課堂討論等，讓孩子在生動的環境中掌握知識。但這一切不能全面預備孩子面向未來，各種課外活動可以彌補這些不足。</p>
+    <div class="acComment container-sm container-md" id="acComment">
+        <div class="acCommentList"  v-for="comment in acComments">
+            <div class="acCommentList_img">
+                <img src="./img/acMain/acComment.jpg" alt="">
+            </div>
+            <div class="acCommentList-text">
+                <h6>{{comment.commenter}} ｜ {{comment.job}}</h6>
+                <p>{{comment.comment}}</p>
+            </div>
         </div>
     </div>
     `,
@@ -22,26 +24,30 @@ Vue.component('acSelectCard', {
     props: { acContents: Array },
     template: `
     <div id="acSelect" class="acSelect container-sm container-md">
-        <div class="acSelectCard" v-for="nums in acContents">
+        <div class="acSelectCard" v-for="acContent in acContents">
                 <a href="./acSelf.html">
                     <img src="./img/acMain/acCard.jpg">
                 </a>
             
             <!-- 卡片文字 -->
-            <h6 class="acSelectCard_title"><a href="./acSelf.html">{{acContents.acName}}</a></h6>
+            <h6 class="acSelectCard_title"><a href="./acSelf.html">{{acContent.acName}}</a></h6>
             
             <!-- 卡片時間 -->
+            <div class="acSelectCard_icon">
+            <img class="acSelectCard-share_icon"src="./img/icon/share.png" alt="">
+            <img class="acSelectCard-bookmark_icon"src="./img/icon/bookmark.png" alt="">
+            </div>
+            <div class="acSelectCard_text">
+            <img class="acSelectCard-time_icon"src="./img/icon/clock.png" alt="">
+            <p class="acSelectCard_time">活動日期：<br>{{acContent.acStartDate}} ~ <br>{{acContent.acEndDate}}</p>
+            </div>
             <div class="acSelectCard_text">
                 <img class="acSelectCard-time_icon"src="./img/icon/clock.png" alt="">
-                <p class="acSelectCard_time">2020-08-15（六）<br> 14:00 - 16:00</p>
-                <div class="acSelectCard_icon">
-                <img class="acSelectCard-share_icon"src="./img/icon/share.png" alt="">
-                <img class="acSelectCard-bookmark_icon"src="./img/icon/bookmark.png" alt="">
-                </div>
+                <p class="acSelectCard_time">報名截止日期：<br>{{acContent.acRedEndDate}}</p>
             </div>
 
             <div class="acSelectCard_bottomBlock">
-            <p class="acSelectCard_person"> 剩餘名額：{{acContents.acMax}}</p>
+            <p class="acSelectCard_person"> 剩餘名額：{{acContent.acMax - acContent.acMin}}</p>
             <input id="acSelectCard_register" type="button" value="立即報名" class="acSelectCard_register">
             </div>
         </div>
@@ -59,9 +65,9 @@ new Vue({
     el: "#acMain",
 
     data: {
-        nums: 6,
         title: ["精選活動", "講座", "療癒", "戶外", "藝文"],
-        contents: [1, 2]
+        contents: [],
+        comments: [],
     },
     methods: {
 
@@ -69,6 +75,10 @@ new Vue({
     mounted() {
         axios.get('./json/acMain.json').then((data) => {
             this.contents = data.data
+        })
+
+        axios.get('./json/acMain_comments.json').then((data) => {
+            this.comments = data.data
         })
     },
     computed: {
