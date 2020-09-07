a74b7828ac52e69dbd08a8425a872ce70cc3edd0
Lu-Cen Jiang <019004@stu.chsc.tw>
2020-09-07 16:17:55 +0800

    no message

--- js/indexVue.js
+++ js/indexVue.js
@@ -1,7 +1,5 @@
-    // vue
-        
-        
-        
-        
-        
-        
+var six = ADDDATE(hiredate, INTERVAL 6 MONTH)
+DATE_FORMAT(
+    ADDDATE(
+        six, INTERVAL MOD(7 - WEEKDAY(six), 7) DAY),
+    '%W ,the %D of %M')
