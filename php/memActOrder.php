29f7e9d40391431490dc94c7ee65d3acfcdf01b3
johsuanhuang <misvamda@gmail.com>
2020-09-12 17:04:04 +0800

    0915-17-02_gulp_fuck_me_up_again

--- php/memActOrder.php
+++ php/memActOrder.php
@@ -3,11 +3,28 @@ try{
     // session_start();
     require_once("./connectBook.php");
 
-    $sql = "select  a.actONo, date(b.actStart) 'actStart', date(b.actEnd) 'actEnd', time(b.actStart) 'actStartTime', time(b.actEnd) 'actEndTime',hour(timediff(time(b.actEnd),time(b.actStart))) 'hour',minute(timediff(time(b.actEnd),time(b.actStart))) 'minute', b.actName, b.actHost, b.actHostTitle , b.actCost ,b.actAddress,c.actPicContent,b.actMin,b.actCount, date(a.actOTime) 'actOTime'
-    from actorder a join activity b using(actNo) left join actPic c using(actNo)
-    where memNo = :memNo
-    group by actONo
-    order by actStart;";
+    $sql = "select a.actONo, 
+                date(b.actStart) 'actStart',
+                date(b.actEnd) 'actEnd', 
+                time(b.actStart) 'actStartTime', 
+                time(b.actEnd) 'actEndTime',
+                hour(timediff(time(b.actEnd),time(b.actStart))) 'hour',
+                minute(timediff(time(b.actEnd),time(b.actStart))) 'minute', 
+                b.actName, 
+                b.actHost, 
+                b.actHostTitle , 
+                b.actCost ,
+                b.actAddress,
+                c.actPicContent,
+                b.actMin,
+                b.actCount,
+                date(a.actOTime) 'actOTime'
+            from actorder a join activity b using(actNo) left join actPic c using(actNo)
+            where memNo = 1 and c.actPicNo =(select min(actPicNo)
+                                        from actPic)
+
+            group by a.actONo
+            order by actStart;";
 
     $memberOrder = $pdo->prepare($sql);
     // 正確做法
@@ -18,7 +35,7 @@ try{
     $memberOrder->execute();
     
     if( $memberOrder->rowCount()==0){ 
-    echo "沒有訂單內容";
+    echo "沒有訂單內容???";
     }else{ 
     $memberOrderAll = $memberOrder->fetchAll(PDO::FETCH_ASSOC);
         echo json_encode($memberOrderAll);
--- php/memArtRec.php
+++ php/memArtRec.php
@@ -3,23 +3,26 @@ try{
     // session_start();
     require_once("./connectBook.php");
 
-    $sql = "select a.actName, 
-    rpad(substr(actContent,1,16),20,'.') 'actContent',
-    date(a.actStart) 'actStart', 
-    c.actPicContent, 
-    group_concat(distinct d.actTypeNo) 'actTypeNo',
-    f.testResultTypeNo
-from activity a join counselor b using(csNo) 
-            join actpic c using(actno) 
-            join activitytype d using(actNo) 
-            join type e on e.typeNo = d.actTypeNo 
-            join testresult f on f.testResultTypeNo = d.actTypeNo 
-            join memtest g using(memTestNo)
-WHERE memNo = 1  and f.testResultTypeNo = (select testResultTypeNo
-                                    from testresult
-                                    having max(testResultValue))
-group by actno 
-order by actStart desc limit 5;";
+    $sql = "
+    select a.artTitle, 
+            b.csName, 
+            rpad(substr(artContent,1,16),20,'.') 'artContent',
+            date(a.artDate) 'artDate', 
+            c.artPicContent , 
+            group_concat(distinct d.artTypeNo) 'artTypeNo',
+            f.testResultTypeNo,
+            f.testResultValue
+    from article a join counselor b using(csNo) 
+                    join artpic c using(artno) 
+                    join articletype d using(artno) 
+                    join type e on e.typeNo = d.artTypeNo 
+                    join testresult f on f.testResultTypeNo = d.artTypeNo 
+                    join memtest g using(memTestNo)
+    WHERE memNo = 1  and  f.testResultValue = (select min(testResultValue)
+                                            from testresult 
+                                            where d.artTypeNo = f.testResultTypeNo)
+    group by artno 
+    order by artDate desc limit 5;";
 
     $memberArtRec = $pdo->prepare($sql);
     // 正確做法
