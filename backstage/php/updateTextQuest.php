757cf8126016f01311c80da1a294ce4bd47cb50f
sponge707 <sbegdhla@gmail.com>
2020-09-16 13:50:30 +0800

    問券抓資料庫

---
+++ backstage/php/updateTextQuest.php
@@ -0,0 +1,35 @@
+<?php
+
+$textNo = $_REQUEST["textNo"];
+$textTypeNo = $_REQUEST["textTypeNo"];
+$textContent = $_REQUEST["textContent"];
+
+
+$errMsg = "";
+//連線資料庫
+try {
+    $dsn = "mysql:host=localhost;port=8889;dbname=ed102g4;charset=utf8";
+    $user = "root";
+    $password = "root";
+    $options = array(PDO::ATTR_CASE => PDO::CASE_NATURAL, PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION);
+    $pdo = new PDO($dsn, $user, $password, $options);
+
+
+    $sql = "UPDATE `text` 
+            SET `textNo` = :textNo,  
+            `textTypeNo` = :textTypeNo, 
+            `textContent` = :textContent, 
+            WHERE (`textNo` = :textNo);
+    ";
+    $textInfo = $pdo->prepare($sql);
+    $textInfo->bindValue(':textNo', $textNo);
+    $textInfo->bindValue(':textTypeNo', $textTypeNo);
+    $textInfo->bindValue(':textContent', $textContent);
+    
+    $textInfo->execute();
+    echo "問券修改成功";
+} catch (PDOException $e) {
+    $errMsg .= "錯誤原因 : " . $e->getMessage() . "<br>";
+    $errMsg .= "錯誤行號 : " . $e->getLine();
+    echo $errMsg;
+}
