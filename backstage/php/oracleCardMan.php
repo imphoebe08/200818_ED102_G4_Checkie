2b0a5df81205c886084a6e056d26dd48d6ca5e96
a0987123023 <a0987123023@gmail.com>
2020-09-17 13:27:00 +0800

    ㄎ

--- backstage/php/oracleCardMan.php
+++ backstage/php/oracleCardMan.php
@@ -1,4 +1,3 @@
-
 <?php
 global $json;
 try{
@@ -7,34 +6,19 @@ try{
     $password = "root";
     $options = array(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
     $pdo = new PDO($dsn, $user, $password, $options);
-  //
-  $deckNo = $_GET["deckNo"];//取得前端送來的資料
-  $deckNo= $_GET["cardNo"];
-  $deckNo= $_GET["cardTitle"];
-  $deckNo= $_GET["cardContent"];
-  $deckNo= $_GET["cardPic"];
-  $deckNo= $_GET["cardTypeNo"];
-  $deckNo= $_GET["deckNo"];
-
-  $sql = "select * from `card`";
-  $cardRow = $pdo->prepare($sql);
-  $cardRow->bindValue(":cardNo", $cardNo);
-  $cardRow->bindValue(":cardTitle", $cardTitle);
-  $cardRow->bindValue(":cardContent", $cardContent);
-  $cardRow->bindValue(":cardPic", $cardPic);
-  $cardRow->bindValue(":cardTypeNo", $cardTypeNo);
-  $cardRow->bindValue(":deckNo", $deckNo);
-  $cardRow->execute();
   
 
-  if( $cardRow->rowCount() ==0){
-    
+  $sql = "
+    select * from card GROUP by deckno; 
+  ";
+  $cardRow = $pdo->query($sql);
+
+  if($cardRow->rowCount()==0){
   }else{
-    $cards = $cardRow->fetchAll(PDO::FETCH_ASSOC);
-    $json = json_encode($cards);
-    echo $json;
-    // $result = array("cardTitle"=>$card["cardTitle"], "cardContent"=>$card["cardContent"],"cardPic"=>$card["cardPic"]);
+    $cardsData = $cardRow->fetchAll(PDO::FETCH_ASSOC);
+    echo json_encode($cardsData);
   }
+    
 }catch(PDOException $e){
   echo "error";
 }
