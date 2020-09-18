af1eb5ef0a6b78e430ceb693cf121df33562fb39
a0987123023 <a0987123023@gmail.com>
2020-09-18 21:54:29 +0800

    card

---
+++ backstage/php/oracleCardAddCard.php
@@ -0,0 +1,35 @@
+<?php
+$cardNo = $_REQUEST["cardNo"];
+$cardTitle = $_REQUEST["cardTitle"];
+$cardContent = $_REQUEST["cardContent"];
+$cardPic = $_REQUEST["cardPic"];
+$cardTypeNo = $_REQUEST["cardTypeNo"];
+$deckNo = $_REQUEST["deckNo"];
+$errMsg = "";
+$errMsg = "";
+//連線資料庫
+try {
+    $dsn = "mysql:host=localhost;port=8889;dbname=ed102g4;charset=utf8";
+    $user = "root";
+    $password = "root";
+    $options = array(PDO::ATTR_CASE => PDO::CASE_NATURAL, PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION);
+    $pdo = new PDO($dsn, $user, $password, $options);
+
+
+    $sql = "INSERT INTO card (cardTitle, cardContent,cardPic,cardTypeNo,deckNo)
+            VALUES (:cardTitle, :cardContent,:cardPic , :cardTypeNo, :deckNo);
+    ";
+    $csInfo = $pdo->prepare($sql);
+    // $csInfo->bindValue(':cardNo', $cardNo);
+    $csInfo->bindValue(':cardTitle', $cardTitle);
+    $csInfo->bindValue(':cardContent', $cardContent);
+    $csInfo->bindValue(':cardPic', $cardPic);
+    $csInfo->bindValue(':cardTypeNo', $cardTypeNo);
+    $csInfo->bindValue(':deckNo', $deckNo);
+    $csInfo->execute();
+    echo "管理員新增成功";
+} catch (PDOException $e) {
+    $errMsg .= "錯誤原因 : " . $e->getMessage() . "<br>";
+    $errMsg .= "錯誤行號 : " . $e->getLine();
+    echo $errMsg;
+}
---
+++ backstage/php/oracleCardDelete.php
@@ -0,0 +1,29 @@
+<?php
+
+$cardNo = $_REQUEST["cardNo"];
+
+$errMsg = "";
+// global $json;
+try{
+    $dsn = "mysql:host=localhost;port=8889;dbname=ed102g4;charset=utf8";
+    $user = "root";
+    $password = "root";
+    $options = array(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+    $pdo = new PDO($dsn, $user, $password, $options);
+  
+
+  $sql = "
+    delete from card where (`cardNo`=:cardNo);
+  ";
+  $cardRow = $pdo->prepare($sql);
+  $cardRow->bindValue(":cardNo", $cardNo);
+  $cardRow->execute();
+  echo "神諭卡刪除成功";
+
+    
+}catch(PDOException $e){
+  $errMsg .= "錯誤原因 : " . $e->getMessage() . "<br>";
+  $errMsg .= "錯誤行號 : " . $e->getLine();
+  echo "error";
+}
+?>
