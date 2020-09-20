2362c6ccbf6239104c149d57932d0749988fe0c0
johsuanhuang <misvamda@gmail.com>
2020-09-20 14:59:35 +0800

    0920_cc_dataBase

--- php/connectBook666.php
+++ php/connectBook666.php
@@ -1,5 +1,5 @@
 <?php
-$dsn="mysql:host=localhost;port=8889;dbname=2;charset=utf8";
+$dsn="mysql:host=localhost;port=8889;dbname=0908Checkie;charset=utf8";
 $user="root";
 $password="root";
 $options=array(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
