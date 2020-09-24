3d2e3c35329a8cc52aa35df38028055b41f202d7
sponge707 <sbegdhla@gmail.com>
2020-09-24 16:29:31 +0800

    cslog csname&cspic

--- backstage/php/csgetLoginInfo.php
+++ backstage/php/csgetLoginInfo.php
@@ -2,7 +2,7 @@
 session_start();
 if( isset($_SESSION["csId"]) === true){
 	//送出登入者的姓名資料
-    $result = array("csId"=>$_SESSION["csId"], "csPsd"=>$_SESSION["csPsd"],"csName"=>$_SESSION["csName"]);
+    $result = array("csId"=>$_SESSION["csId"], "csPsd"=>$_SESSION["csPsd"],"csName"=>$_SESSION["csName"],"csPic"=>$_SESSION["csPic"]);
     echo json_encode($result);
 }else{
 	echo "{}";
