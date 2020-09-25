667f56275825e793be5e6a6bfa6aef791dbecc05
Phoebe Tsai <imphoebe08@gmail.com>
2020-09-25 19:01:15 +0800

    專欄後台圖片

--- backstage/php/uploadArtPic.php
+++ backstage/php/uploadArtPic.php
@@ -5,23 +5,30 @@ if ($_FILES["file"]["error"] > 0) {
 } else {
     $from = $_FILES["file"]["tmp_name"];
     if($_REQUEST["picNo"] == 1){
-        $to = "../../img/atSelf/" . "banner" . $_FILES["file"]["name"];
-        $to2 = "../img/atSelf/" . "banner" . $_FILES["file"]["name"];
-    }elseif($_REQUEST["picNo"] == 2){
-        $to = "../../img/atSelf/" . "pic2" . $_FILES["file"]["name"];
-        $to2 = "../img/atSelf/" . "pic2" . $_FILES["file"]["name"];
-    }elseif($_REQUEST["picNo"] == 3){
-        $to = "../../img/atSelf/" . "pic3" . $_FILES["file"]["name"];
-        $to2 = "../img/atSelf/" . "pic3" . $_FILES["file"]["name"];
-    }else{
-        $to = "../../img/atSelf/" . "pic4" . $_FILES["file"]["name"];
-        $to2 = "../img/atSelf/" . "pic34" . $_FILES["file"]["name"];
-    };
+        $to = "../img/atSelf/" . "banner" . $_FILES["file"]["name"];
+        $to2 = "../../img/atSelf/" . "banner" . $_FILES["file"]["name"];
+        $to3 = "./img/atSelf/" . "banner" . $_FILES["file"]["name"];
+        // echo"$to3";
 
+    }elseif($_REQUEST["picNo"] == 2){
+        $to = "../img/atSelf/" . "pic2" . $_FILES["file"]["name"];
+        $to2 = "../../img/atSelf/" . "pic2" . $_FILES["file"]["name"];
+        $to3 = "./img/atSelf/" . "pic2" . $_FILES["file"]["name"];
+        // echo"$to3";
+
+    }elseif($_REQUEST["picNo"] == 3){
+        $to = "../img/atSelf/" . "pic3" . $_FILES["file"]["name"];
+        $to2 = "../../img/atSelf/" . "pic3" . $_FILES["file"]["name"];
+        $to3 = "./img/atSelf/" . "pic3" . $_FILES["file"]["name"];
+        // echo"$to3";
+
+    }
     copy($from, $to);
     copy($from, $to2);
+    // copy($from, $to3);
 
-    echo "$to";
+
+    echo $to3;
 };
 
 ?>
