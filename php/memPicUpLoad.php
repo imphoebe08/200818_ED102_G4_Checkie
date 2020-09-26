c4b52e01abe8222866b4da73ac6e74b3f7dd9817
johsuanhuang <misvamda@gmail.com>
2020-09-26 15:48:51 +0800

    0926_cc_1548

--- php/memPicUpLoad.php
+++ php/memPicUpLoad.php
@@ -5,12 +5,14 @@
             echo "Error:" . $_FILES["memPic"]["error"];
         } else {
             $from = $_FILES["memPic"]["tmp_name"];
-            $to = "./img/user/" . $_FILES["memPic"]["name"];
-            $to2 = '../backstage/img/member/'.$_FILES["memPic"]["name"];
+            $to = "../img/user/" . $_FILES["memPic"]["name"];
+            
+            $to2 = '../backstage/img/user/'.$_FILES["memPic"]["name"];
+            $to3 = "./img/user/" . $_FILES["memPic"]["name"];
             copy($from, $to);
             copy($from, $to2);
         
-            echo "$to";
+            
         }
     
         $sql = "UPDATE `member` SET `memPic` = :memPic 
@@ -19,9 +21,10 @@
         $memberOrder = $pdo->prepare($sql);
 
     
-        $memberOrder->bindValue(":memPic", $to);
+        $memberOrder->bindValue(":memPic", $to3);
         $memberOrder->bindValue(":memNo", $_POST["memNo"]);
         $memberOrder->execute();
+        echo 'aaaa';
     
         }catch(PDOException $e){
             echo $e->getMessage();
